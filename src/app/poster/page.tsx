a0f0a9d5dd699b17c0973f891a189e663495da7d
sean2474 <sean24740@gmail.com>
2023-08-07 13:45:41 +0900

    added button in poster

--- src/app/poster/page.tsx
+++ src/app/poster/page.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Button } from '@/components/button/button'
 
 export default function Home() {  
   return (
     <main className={styles.main}>
       <div className={styles.poster}>
         <Image src="/images/poster.jpg" alt="poster" width={500} height={500} layout="responsive" />
+      </div>
+      <div className={styles.posterButton}>
+        <Button href='/'>되돌아가기</Button>
       </div>
     </main>
   )
